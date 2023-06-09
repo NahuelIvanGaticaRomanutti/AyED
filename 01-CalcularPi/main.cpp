f68e2ba7bbd597712e97d0297c9049acd7041013
Nahuel Gatica <ngatica@frba.utn.edu.ar>
2023-06-08 22:01:31 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -2,16 +2,23 @@
 #include <iomanip>
 using namespace std;
 
-int main() {
-double pi = 0.0;
-int it = 1000000;
-for (int i = 0 ; i < it ; ++i) {
-double num;
-i%2 !=0 ? num = -1 : num = 1;
-double den = 2*i+1;
-pi += num / den; 
-}
-pi *= 4;
-cout << "El valor aproximado de pi, con 6 decimales, es: " << setprecision(7) << pi << endl;
-return 0;
+int main () {
+    double pi {0.0};
+    int i {0};
+    for (;;++i) {
+        double num;
+        i % 2 == 0 ? num = 1 : num = -1;
+        double den = 2 * i + 1;
+        pi += num / den;
+        double V {pi * 4 - 3.141592};
+        if (V < 0.0) {
+            V = -V;
+        }
+        if (V < 0.0000001) {
+            break;
+        }
+    }
+    pi *= 4;
+    cout << "Este es el valor de pi: " << setprecision(7) << pi << endl;
+    return 0;
 }
